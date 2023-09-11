023a089f501e4bf933f97053399cdc253eec919b
caceres90 <caceresmartinezjhonalexander@gmail.com>
2023-09-10 23:30:02 -0500

    Jhon create description to products and user form

---
+++ usuario/js/form.js
@@ -0,0 +1,26 @@
+function showform(){
+  var formulario = document.getElementById('Formulario');
+  formulario.style.display = "block";  
+}
+
+document.querySelector('form').addEventListener('submit', isValid)
+
+function isValid(e){
+    e.preventDefault();
+    var email = document.getElementById("email1");
+    console.log(email);
+    var username = document.getElementById("username1");
+    console.log(username);
+    var password1 = document.getElementById("Password1");
+    var password2 = document.getElementById("Password2");
+    var description = document.getElementById("description1");
+    console.log(description);
+    
+    if (password1 == password2){
+    password = password1;
+    console.log(password1);
+    console.log(password2);
+    }else{
+      prompt("La contraseña no coincide");
+    }
+}
